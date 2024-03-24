c961d7103daa49945373abc1982d9cfe36f08719
RujuNaik26 <127389701+RujuNaik26@users.noreply.github.com>
2024-03-24 20:47:43 +0530

    Update DBScript.sql

--- DB_Script/DBScript.sql
+++ DB_Script/DBScript.sql
@@ -1,7 +1,6 @@
 USE [master]
 GO
-
-/****** Object:  Database [Jainam_DB]    Script Date: 24-03-2024 19:30:19 ******/
+/****** Object:  Database [Jainam_DB]    Script Date: 24-03-2024 20:49:08 ******/
 CREATE DATABASE [Jainam_DB]
  CONTAINMENT = NONE
  ON  PRIMARY 
@@ -10,106 +9,153 @@ CREATE DATABASE [Jainam_DB]
 ( NAME = N'Jainam_DB_log', FILENAME = N'C:\Users\91972\Jainam_DB_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
  WITH CATALOG_COLLATION = DATABASE_DEFAULT
 GO
-
+ALTER DATABASE [Jainam_DB] SET COMPATIBILITY_LEVEL = 150
+GO
 IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
 begin
 EXEC [Jainam_DB].[dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
-
 ALTER DATABASE [Jainam_DB] SET ANSI_NULL_DEFAULT OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET ANSI_NULLS OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET ANSI_PADDING OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET ANSI_WARNINGS OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET ARITHABORT OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET AUTO_CLOSE OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET AUTO_SHRINK OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET AUTO_UPDATE_STATISTICS ON 
 GO
-
 ALTER DATABASE [Jainam_DB] SET CURSOR_CLOSE_ON_COMMIT OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET CURSOR_DEFAULT  GLOBAL 
 GO
-
 ALTER DATABASE [Jainam_DB] SET CONCAT_NULL_YIELDS_NULL OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET NUMERIC_ROUNDABORT OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET QUOTED_IDENTIFIER OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET RECURSIVE_TRIGGERS OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET  DISABLE_BROKER 
 GO
-
 ALTER DATABASE [Jainam_DB] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET DATE_CORRELATION_OPTIMIZATION OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET TRUSTWORTHY OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET ALLOW_SNAPSHOT_ISOLATION OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET PARAMETERIZATION SIMPLE 
 GO
-
 ALTER DATABASE [Jainam_DB] SET READ_COMMITTED_SNAPSHOT OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET HONOR_BROKER_PRIORITY OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET RECOVERY SIMPLE 
 GO
-
 ALTER DATABASE [Jainam_DB] SET  MULTI_USER 
 GO
-
 ALTER DATABASE [Jainam_DB] SET PAGE_VERIFY CHECKSUM  
 GO
-
 ALTER DATABASE [Jainam_DB] SET DB_CHAINING OFF 
 GO
-
 ALTER DATABASE [Jainam_DB] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
 GO
-
 ALTER DATABASE [Jainam_DB] SET TARGET_RECOVERY_TIME = 60 SECONDS 
 GO
-
 ALTER DATABASE [Jainam_DB] SET DELAYED_DURABILITY = DISABLED 
 GO
-
 ALTER DATABASE [Jainam_DB] SET ACCELERATED_DATABASE_RECOVERY = OFF  
 GO
-
 ALTER DATABASE [Jainam_DB] SET QUERY_STORE = OFF
 GO
+USE [Jainam_DB]
+GO
+/****** Object:  Table [dbo].[EmployeeDetail]    Script Date: 24-03-2024 20:49:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[EmployeeDetail](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[EmployeeId] [varchar](50) NULL,
+	[EmployeeName] [varchar](50) NULL,
+	[Department] [varchar](50) NULL,
+	[EmployeeEmail] [varchar](100) NULL,
+	[JobTitle] [varchar](100) NULL,
+	[PhoneNumber] [varchar](100) NULL,
+	[UpdatedBy] [varchar](100) NULL,
+ CONSTRAINT [PK_EmployeeDetail] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[EmployeeLogin]    Script Date: 24-03-2024 20:49:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[EmployeeLogin](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[LoginId] [varchar](50) NULL,
+	[Password] [varchar](50) NULL,
+	[EmployeeName] [varchar](50) NULL,
+ CONSTRAINT [PK_EmployeeLogin] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  StoredProcedure [dbo].[InsertOrUpdateEmployee]    Script Date: 24-03-2024 20:49:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[InsertOrUpdateEmployee]
+    @Id INT = 0,
+    @EmployeeId VARCHAR(100)='',
+    @EmployeeName VARCHAR(100)='',
+    @Department VARCHAR(50)='',
+	@EmployeeEmail VARCHAR(100)='',
+	@JobTitle VARCHAR(100)='',
+	@PhoneNumber VARCHAR(100)='',
+	@UpdatedBy VARCHAR(100)=''
+AS
+BEGIN
+    SET NOCOUNT ON;
 
+    IF EXISTS (SELECT 1 FROM EmployeeDetail WHERE Id = @Id)
+    BEGIN
+        -- Update existing record
+        UPDATE EmployeeDetail
+        SET EmployeeId = @EmployeeId,
+			EmployeeName = @EmployeeName,
+            Department = @Department,
+			EmployeeEmail = @EmployeeEmail,
+			JobTitle = @JobTitle,
+			PhoneNumber = @PhoneNumber,
+			UpdatedBy = @UpdatedBy
+        WHERE Id = @Id;
+    END
+    ELSE
+    BEGIN
+        -- Insert new record
+        INSERT INTO EmployeeDetail (EmployeeId, EmployeeName, Department,EmployeeEmail,JobTitle,PhoneNumber)
+        VALUES (@EmployeeId, @EmployeeName, @Department,@EmployeeEmail,@JobTitle,@PhoneNumber);
+    END;
+END;
+GO
+USE [master]
+GO
 ALTER DATABASE [Jainam_DB] SET  READ_WRITE 
 GO
-
